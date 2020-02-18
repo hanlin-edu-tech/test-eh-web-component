78acbac3eee5bf69db4c9b30da911c87b6ce3b77
hotdog929 <hotdog929@gmail.com>
2020-02-18 17:10:42 +0800

    抽出 eh-header 到獨立的 js

---
+++ eh-header.js
@@ -0,0 +1,25 @@
+{
+    const template = `
+            <style>
+                .logo {
+                    width: 304px;
+                    height: 35px;
+                }
+            </style>
+            <header>
+                <a href="/index.html"><img class="logo" src="https://www.ehanlin.com.tw/images/ehanlin_logo.svg"/></a>
+                <nav>
+                    <a>購物車</a> | <a>課程開通</a> | <a>免費試用</a> | <a>使用幫手</a>
+                </nav>
+            </header>
+    `;
+
+    customElements.define('eh-header', class extends HTMLElement {
+        constructor() {
+            super();
+            let shadowRoot = this.attachShadow({mode: 'open'});
+            shadowRoot.innerHTML = template;
+        }
+    });
+
+}
