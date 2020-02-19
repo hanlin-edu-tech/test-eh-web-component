ce047352c2194d66d11fea4dff9d3f20eff55784
hotdog929 <hotdog929@gmail.com>
2020-02-19 15:26:21 +0800

    調整 程式碼排版

--- dataprovider.js
+++ dataprovider.js
@@ -1,5 +1,6 @@
 {
     const viewItemsData = [];
+    //封鎖資料物件的 array 修改
     const viewItemsProxy = new Proxy(viewItemsData, {
         set : (target, prop, value) => {
             throw new TypeError('Can not change iteams');
@@ -19,6 +20,39 @@
         dispatchEvent(new Event("items-change"))
     };
 
+    const viewData = {
+        page : -1,
+        items : viewItemsProxy
+    }
+    const viewProxy = new Proxy(viewData, {
+        set : (target, prop, value) => {
+            //封鎖 items 的修改
+            if(prop == "items"){
+                throw new TypeError('Can not set iteams');
+            }
+            target[prop] = value;
+            //若是設定 page 重新整理 items
+            if(prop == "page"){
+                updateItemsData(value);
+            }
+        },
+        get : (target, prop, receiver) => {
+            //轉接 addEventListener
+            if(prop == "addEventListener"){
+                return addEventListener;
+            }else{
+                return Reflect.get(target, prop, receiver);
+            }
+        }
+    })
+
+    //開放給頁面使用的介面
+    window.EHDataprovider = {
+        view : viewProxy
+    }
+
+
+    //以下為和主要邏輯無關的工具類    
     const listeners = [];
     const addEventListener = (type, callback) => {
         if (!(type in listeners)) {
@@ -38,30 +72,4 @@
         return !event.defaultPrevented;
     };
 
-    const viewData = {
-        page : -1,
-        items : viewItemsProxy
-    }
-    const viewProxy = new Proxy(viewData, {
-        set : (target, prop, value) => {
-            if(prop == "items"){
-                throw new TypeError('Can not set iteams');
-            }
-            target[prop] = value;
-            if(prop == "page"){
-                updateItemsData(value);
-            }
-        },
-        get : (target, prop, receiver) => {
-            if(prop == "addEventListener"){
-                return addEventListener;
-            }else{
-                return Reflect.get(target, prop, receiver);
-            }
-        }
-    })
-
-    window.EHDataprovider = {
-        view : viewProxy
-    }
 }
